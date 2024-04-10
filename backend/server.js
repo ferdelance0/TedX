987e92f37beab29a35810f1497d5659a9bc883e1
Akhil Hakkim <akhilhakkim@gmail.com>
2024-04-10 14:10:52 +0530

    events being displayed in admin dashboard succesfully, previous problems yet to be solved

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Event = require('./models/events.model');
-const SubEvent = require('./models/subevents.model');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const Event = require("./models/events.model");
+const SubEvent = require("./models/subevents.model");
+const cors = require("cors");
 
 // Create Express app
 const app = express();
@@ -10,19 +10,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 // MongoDB Atlas connection string
-const mongoURI = 'mongodb://localhost:27017/event_management';
+const mongoURI = "mongodb://localhost:27017/event_management";
 
-app.get('/createevents', async (req, res) => {
+app.get("/events", async (req, res) => {
   try {
-    console.log('haaiiiii');
+    const events = await Event.find();
+    res.status(200).json(events);
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 });
 
-app.post('/createevents', async (req, res) => {
+app.post("/createevents", async (req, res) => {
   try {
-    console.log('haaiiiii');
+    console.log("haaiiiii");
     console.log(req.body);
     const event = await Event.create(req.body);
     console.log(event);
@@ -33,7 +34,7 @@ app.post('/createevents', async (req, res) => {
   }
 });
 
-app.post('/createsubevents', async (req, res) => {
+app.post("/createsubevents", async (req, res) => {
   try {
     const event = await SubEvent.create(req.body);
     res.status(200).json({ event });
@@ -48,6 +49,6 @@ mongoose
   .then(() => {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   })
   .catch((err) => console.error(err));
