7f5871c4ca9e17398152673051c096e1d98c3012
ferdelance0 <doncdelish0@gmail.com>
2024-04-14 11:31:46 +0530

    ID Card Gen

--- backend/certificate-gen/generatecertificate.js
+++ backend/certificate-gen/generatecertificate.js
@@ -17,9 +17,8 @@ const firebaseConfig = {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const storage = getStorage()
-// Register fontkit with PDFDocument
 
-const generatePDF = async (name) => {
+const generateCertificatePDF = async (name) => {
     const existingPdfBytes = fs.readFileSync("./template/Dark Blue and Gold Elegant Certificate of Achievement Template (1).pdf");
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
     pdfDoc.registerFontkit(fontkit);
@@ -57,7 +56,7 @@ const generatePDF = async (name) => {
     // Save the modified PDF
     const pdfBytes = await pdfDoc.save();
     fs.writeFileSync("output.pdf", pdfBytes);
-    const pdfRef = ref(storage, `${name}.pdf`);
+    const pdfRef = ref(storage, `Certs/${name}.pdf`);
     await uploadBytes(pdfRef, pdfBytes);
     const downloadURL = await getDownloadURL(pdfRef);
 
@@ -65,4 +64,58 @@ const generatePDF = async (name) => {
     return (downloadURL);
 };
 
-module.exports = generatePDF;
+const generateIDPDF = async (name) => {
+    const position = "Attendee";
+    const existingPdfBytes = fs.readFileSync("./template/White Creative Business Card Template.pdf");
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    pdfDoc.registerFontkit(fontkit);
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+
+    // Load the downloaded font file
+    const fontBytes = fs.readFileSync("./template/FjallaOne-Regular.ttf");
+
+    // Embed the font into the PDF document
+    const font = await pdfDoc.embedFont(fontBytes);
+
+    // Calculate the width of the text
+    const fontSize = 18; // Adjust the font size as needed
+    const nameWidth = font.widthOfTextAtSize(name, fontSize);
+
+    // Calculate the horizontal position for centering the text
+    const pageWidth = firstPage.getWidth();
+    const horizontalPosition = ((pageWidth - nameWidth) / 2)-60;
+
+    // Calculate the vertical position for centering the text
+    const pageHeight = firstPage.getHeight();
+    const textHeight = fontSize;
+    const verticalPosition = (pageHeight - textHeight) / 2;
+
+    // Draw the text on the page
+    firstPage.drawText(name, {
+        x: horizontalPosition,
+        y: verticalPosition + 20, // Adjust vertical position as needed
+        size: fontSize,
+        color: rgb(0, 0, 0),
+        font: font,
+    });
+    firstPage.drawText(position, {
+        x: horizontalPosition,
+        y: verticalPosition -1, // Adjust vertical position as needed
+        size: fontSize,
+        color: rgb(0, 0, .5),
+        font: font,
+    });
+
+    // Save the modified PDF
+    const pdfBytes = await pdfDoc.save();
+    fs.writeFileSync("output.pdf", pdfBytes);
+    // const pdfRef = ref(storage, `ID/${name}.pdf`);
+    // await uploadBytes(pdfRef, pdfBytes);
+    // const downloadURL = await getDownloadURL(pdfRef);
+
+    // console.log("PDF generated successfully.");
+    // return (downloadURL);
+};
+
+module.exports = {generateCertificatePDF,generateIDPDF};
